f0bc5c0a6e659c42a6041c48890c2e78022c81ac
Cheetah <unconfigured@null.spigotmc.org>
2025-08-05 19:27:33 -0700

    feat: undo the last 3 days of progress in preperation for funnies

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,6 @@
-use crate::app::App;
+use anyhow::Result;
 
-pub mod app;
-pub mod cli;
-pub mod config;
-pub mod logging;
-
-pub fn run() -> anyhow::Result<()> {
-    let cli = cli::parse();
-    config::initialize_config(&cli)?;
-    logging::initialize_logging(cli.log_to_stdio)?;
-
-    if cli.print_config {
-        println!("{}", toml::to_string_pretty(config::get())?);
-    }
-
-    println!("hi there this is a testyh thingiweoruiwo34ioiw3u4owi3u4o");
-    println!("config: {:?}", config::get());
-    let mut app = App::new()?;
-    app.run()
+pub fn run() -> Result<()> {
+    println!(":D");
+    Ok(())
 }
